084d5f790ce14d97e47391d7e154528190c077e3
Maryann Jordan <majch@DESKTOP-KQJEU0U.(none)>
2018-03-11 11:54:49 -0500

    initial files

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+use burgers_db;
+INSERT INTO burgers (burger_name) VALUES ('Onionhead');
+INSERT INTO burgers (burger_name) VALUES ('Cheesey');
+INSERT INTO burgers (burger_name) VALUES ('Plain');
+INSERT INTO burgers (burger_name) VALUES ('Bacon');
